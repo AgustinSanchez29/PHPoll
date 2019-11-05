03b3f2b40903f3cd265534b742d1d8e7a2000ce2
Agustin <agustin.s2966@gmail.com>
2019-11-04 22:21:17 -0500

    Se le agrego las funciones de eliminar y editar y se agregaron campos que contienen el id de las preguntas y respuestas para llevar el control de las funciones

--- admin.php
+++ admin.php
@@ -3,6 +3,8 @@
 <div class="container p-4">
 <h3>Pantalla de Administrador</h3>
     <div class="row">
+    <!-- Esta parte imprime una card que  obtiene el numero de preguntas por respuesta, al darle enviar te enviara
+    a otra ventana con otro formulario-->
         <div class="col-md-12">
 
             <div class="card card-body">
@@ -16,35 +18,46 @@
             </div>
         
         </div>
+
+        <!-- Esta parte imprime CARDS con las preguntas con sus respectivas respuestas, te permitira ahi mismo editarla y eliminarlas -->
             <?php $query="SELECT * from preguntas";
             $res=mysqli_query($con,$query);
             
-            while($var=mysqli_fetch_array($res)){
+            while($var=mysqli_fetch_array($res)){    
             $idPreguntas=$var["id_preguntas"];?>
             
             <div class="col-md-6 mt-5">
-                <form action="procesar" method="POST">
+            <div class="card card-body">
+                <form action="procesar.php" method="POST">
                     <div class="form-group">
-                    <input type="text" name="var" class="form-control" value="<?php echo $var["descripcion"];?>">
+                    <input type="text" name="pregunta" class="form-control" value="<?php echo $var["descripcion"];?>">
+                    </div>
+                    <div class="form-group">
+                    <textarea name="idpreguntas" rows="2" class="form-control"
+                    placeholder="ID: <?php echo $idPreguntas ?>"></textarea>    
                     </div>
                     <?php $query2="SELECT * FROM respuestas where id_preguntas= $idPreguntas";
                             $res2=mysqli_query($con,$query2);  
+                            $contador=1; 
                     while($var2=mysqli_fetch_array($res2)){
+                       
                      $idRespuestas=$var2["id_respuestas"];?>
-            
+
                     <div class="form-group">
-                    <textarea name="description" rows="2" class="form-control"
-                    placeholder="<?php echo $var2["descripcion"]?>"></textarea>    
+                    <textarea name="descripcion[<?php echo $contador;?>]" rows="2" class="form-control"
+                    placeholder="<?php echo $var2["descripcion"];?>"></textarea>    
                     </div>
-            <?php }?>
+
                     <div class="form-group">
-                    <textarea name="idpreguntas" rows="2" class="form-control"
-                    placeholder="<?php echo $idPreguntas ?>"></textarea>    
+                    <textarea name="idResp[<?php echo $contador;?>]" rows="2" class="form-control"
+                    placeholder="ID: <?php echo $idRespuestas;?>"></textarea>    
                     </div>
+            <?php $contador++; }?>
+                    
                     <input type="submit" class="btn btn-info btn-block" name="editar" value="Editar">
                     <input type="submit" class="btn btn-danger btn-block" name="eliminar" value="Eliminar">
                 </form>
-
+            </div>
             </div>
             
             <?php }?>
--- procesar.php
+++ procesar.php
@@ -1,15 +1,33 @@
 <?php
-
+include("conexion.php");
+$id=$_POST["idpreguntas"];
 if($_POST["editar"]){
-
-
+    $pregunta=$_POST["pregunta"];
+    $query="UPDATE preguntas set descripcion='$pregunta' where id_preguntas='$id'";
+    $res=mysqli_query($con,$query);
+    if(!$res){
+        die("ERROR-->query fail resp");
+    }   
+    for($i=0;$i<sizeof($_REQUEST["descripcion"]);$i++){
+        $tempId= $_REQUEST["idResp"][$i+1];
+        $tempDes= $_REQUEST["descripcion"][$i+1];
+        $query2= "UPDATE respuestas set descripcion='$tempDes' where id_respuestas='$tempId'";
+        $res2= mysqli_query($con,$query2);
+        if(!$res2){
+            die("ERROR-->query fail");
+        }   
+    }
 
 }
 
-if($_POST["eliminar"]){
-    $id=$_POST["idpreguntas"];
-    
+elseif($_POST["eliminar"]){
+    $query= "DELETE FROM preguntas WHERE id_preguntas= $id";
+    $res=mysqli_query($con,$query);
+    if(!$res){
+        die("query failed");
+    }
 }
+header("location:admin.php");
 
 
 
